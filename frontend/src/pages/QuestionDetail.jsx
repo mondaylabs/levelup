16c35ee8d34138e30500b79641b29c18fa561ff9
shakhbozv <shakhbozv@gmail.com>
2023-10-13 17:28:03 +0500

    frontend

---
+++ frontend/src/pages/QuestionDetail.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function QuestionDetail({}) {
+    return (
+        // <div className="flex">
+        //     <div className="w-[20%] bg-green-50 h-[100vh] flex justify-center items-center p-[30px]">
+        //
+        //     </div>
+        // </div>
+        <div className="flex flex-col w-full lg:flex-row">
+  <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div>
+
+</div>
+    );
+}
+
+export default QuestionDetail;
